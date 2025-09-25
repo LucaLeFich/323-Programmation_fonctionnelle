148dcfcce13d251b1247655de783c982c9f3fd38
LucaLeFich <pq48wkn@eduvaud.ch>
2025-09-25 10:33:22 +0200

    exercice Rando

--- exos/rando/RandoBase/Rando/Rando.cs
+++ exos/rando/RandoBase/Rando/Rando.cs
@@ -1,54 +1,54 @@
+using Gpx;
+ 
 namespace Rando
 {
     public partial class Rando : Form
     {
-        string gpxFilePath = @"..\..\gpx\Ballade_chataignere.gpx";
+        string gpxFile = @"..\..\..\gpx\Ballade_chataignere.gpx";
+        List<TrackPoint> trackPoints = new();
+
         public Rando()
         {
             InitializeComponent();
 
-            if(!File.Exists(gpxFile))
+            if (!File.Exists(gpxFile))
             {
                 MessageBox.Show($"Fichier {gpxFile} non trouvé !!");
             }
 
-            StreamReader streamReader = new StreamReader(gpxFilePath);
+            StreamReader streamReader = new StreamReader(gpxFile);
 
-            List<TrackPoint> points = new();
-            using (GpxReader reader = new GpxReader(streamReader))
+
+            using (GpxReader reader = new GpxReader(streamReader.BaseStream))
             {
                 while (reader.Read())
                 {
                     switch (reader.ObjectType)
                     {
-                        case GpxObjectType.Metadata:
-                            //writer.WriteMetadata(reader.Metadata);
-                            break;
-                        case GpxObjectType.WayPoint:
-                            //writer.WriteWayPoint(reader.WayPoint);
-                            break;
-                        case GpxObjectType.Route:
-                            //writer.WriteRoute(reader.Route);
-                            break;
                         case GpxObjectType.Track:
                             //writer.WriteTrack(reader.Track);
                             var gpxPoints = reader.Track.ToGpxPoints();
- 
+
                             //TODO convertir les gpxPoints en points
                             //avec un SELECT ;-)
-                            gpxPoints.Select(gpxPoint => new TrackPoint()
-                            {
-                                Elevation = gpxPoint.Elevation,
-                                Latitude = gpxPoint.Latitude,
-                                Longitude = gpxPoint.Longitude
-                            });
+                            var converted = gpxPoints
+                                .Select(gpxPoint => new TrackPoint()
+                                {
+                                    Elevation = gpxPoint.Elevation,
+                                    Latitude = gpxPoint.Latitude * 10000,
+                                    Longitude = gpxPoint.Longitude * 10000
+                                });
 
-                            points.AddRange(converted.ToList());
+                            trackPoints.AddRange(converted.ToList());
 
                             break;
                     }
                 }
+
             }
+
+
+
         }
 
         private void Rando_Form_Paint(object sender, PaintEventArgs e)
@@ -56,7 +56,28 @@ namespace Rando
             Pen myPen = new Pen(Color.Red);
             myPen.Width = 2;
 
-            Point[] points = new Point[4] { new Point(30,50), new Point(50,10), new Point(80,50), new Point(111,400) };
+            var minLat = trackPoints.Min(tp => tp.Latitude);
+            var maxLat = trackPoints.Max(tp => tp.Latitude);
+            var rangeLat = maxLat - minLat;
+            var ratioLat = rangeLat / Width;
+
+            var minLong = trackPoints.Min(tp => tp.Longitude);
+            var maxLong = trackPoints.Max(tp => tp.Longitude);
+            var rangeLong = maxLong - minLong;
+            var ratioLong = rangeLong / Height;
+
+            //De trackpoint VERS POINT
+            Point[] points = trackPoints
+                .Select(trackPoint => new Point()
+                {
+                    X = Convert.ToInt32((trackPoint.Latitude - minLat) * ratioLat),
+                    Y = Convert.ToInt32((trackPoint.Longitude - minLong) * ratioLong)
+                }
+                )
+                .ToArray();
+
+
+
             this.CreateGraphics().DrawLines(myPen, points);
         }
     }
