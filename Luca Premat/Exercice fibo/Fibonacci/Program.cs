4ef87b727189f28f28a8486bdd38a95862a64605
LucaLeFich <pq48wkn@eduvaud.ch>
2025-10-30 11:59:28 +0100

    Exercices

---
+++ Luca Premat/Exercice fibo/Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+static int Fibonacci(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    n = Fibonacci(n - 1) + Fibonacci(n - 2);
+    return n;
+}
+//Console.WriteLine(Fibonacci(13));
+
+static void main(int x)
+{
+    while (x > 0)
+    {  
+        x = x - 1;
+        Console.WriteLine(Fibonacci(x));
+    }
+}
+main(13);
+
+
+// ordre croissant
+static void main2(int x)
+{
+    int y = -1;
+    while (y < x - 1)
+    {
+        y = y + 1;
+        Console.WriteLine(Fibonacci(y));
+    }
+}
+//main2(13);
---
+++ Luca Premat/Exercice puissance/Puissance/Program.cs
@@ -0,0 +1,8 @@
+﻿static double Power(double baseNumber, int exponent)
+{
+    if (exponent == 0)
+        return 1;
+
+    return baseNumber * Power(baseNumber, exponent - 1);
+}
+Console.WriteLine(Power(2, 3));
